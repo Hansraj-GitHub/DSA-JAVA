7682a3d0c48239845a8384d939038f1b2ef414e9
Hansraj-GitHub <hraj2662001@gmail.com>
2024-08-20 21:19:35 +0530

    BetterSolution

--- secondLargest.java
+++ secondLargest.java
@@ -1,28 +1,16 @@
 public class secondLargest {
     public static void main(String[] args) {
-        int[] arr = {5, 16, 12, 6, 9};
-        
-        if (arr.length < 2) {
-            System.out.println("Array doesn't have enough elements to find the second largest.");
-            return;
-        }
-        
-        int largest = Integer.MIN_VALUE;
-        int secondLargest = Integer.MIN_VALUE;
-        
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > largest) {
-                secondLargest = largest; 
+        int arr[] = { 10,7,15,9,25,80,70,777,526,190};
+        int largest = arr[0];
+        int secondLargest= -1;
+        for(int i = 0; i < arr.length; i++) {
+            if ( arr[i] > largest ) {
+                secondLargest=largest;
                 largest = arr[i];
-            } else if (arr[i] > secondLargest && arr[i] != largest) {
-                secondLargest = arr[i];
+            } else if (arr[i] < largest && arr[i] > secondLargest ) {
+                secondLargest=arr[i];
             }
-        }
-        
-        if (secondLargest == Integer.MIN_VALUE) {
-            System.out.println("There is no second largest element in the array");
-        } else {
-            System.out.println("The second largest element is: " + secondLargest);
-        }
+        }  
+        System.out.println("secondLargest no is: "+ secondLargest);
     }
 }
