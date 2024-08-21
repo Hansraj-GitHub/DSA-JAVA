a7ed3a23adcd1d0ca688b1381b75290edf93f471
Hansraj-GitHub <hraj2662001@gmail.com>
2024-08-21 09:19:17 +0530

    missingNo

--- missingNumber.java
+++ missingNumber.java
@@ -2,17 +2,17 @@
 
 public class missingNumber {
     public static void main(String[] args) {
-       int arr[] = {1, 3, 4, 5};
+       int arr[] = {1,2,3,5};
        int n = 5;
-        for( int i=0; i<=arr.length;i++) {
-            
-           System.out.println(i);
+       int missingNumber = 0;
+       int expectedsum = n * (n+1) / 2;
+       int actualsum = 0;
+
+        for( int i = 0 ; i < arr.length ; i++) {
+            actualsum = actualsum + arr[i];
         }
-        
-        
-
-        
-
-    }
+        missingNumber = expectedsum - actualsum;
+        System.out.println(missingNumber);
+    }       
     
 }
