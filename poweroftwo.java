a937d78fbb4e321326db23da7da51ca016c4602e
Hansraj-GitHub <hraj2662001@gmail.com>
2024-08-26 09:24:09 +0530

    power of two

---
+++ poweroftwo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class poweroftwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any no :");
+        int n = sc.nextInt();
+        boolean isPowerOfTwo = n > 0;
+        while (n > 1 && n % 2 == 0) {
+            n = n / 2;
+        }
+        isPowerOfTwo = isPowerOfTwo && (n == 1);
+        System.out.println(isPowerOfTwo);
+    }
+}
