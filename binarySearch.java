029a4f20f3bc7bf328f36901949b591d47e4e17c
Hansraj-GitHub <hraj2662001@gmail.com>
2024-08-27 16:42:35 +0530

    binary search

---
+++ binarySearch.java
@@ -0,0 +1,17 @@
+
+public class stringExample {
+    public static void main(String[] args) {
+        int[] arr = {1,5,6,7,9};
+        int target = 10;
+
+        int index = -1;
+        for ( int i = 0; i < arr.length ; i++){
+            if ( arr[i] == target){
+                index = i;
+                break;
+            }
+        }
+
+        System.out.println(index);
+    }
+}
