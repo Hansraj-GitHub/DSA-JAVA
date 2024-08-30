49223af23f3af2be5618793c0cf32143e4de59e9
Hansraj-GitHub <hraj2662001@gmail.com>
2024-08-30 17:41:57 +0530

    Addpattern

--- pattern.java
+++ pattern.java
@@ -16,10 +16,9 @@ public class pattern {
             }
             System.out.println();
         } 
-    }
-}
+ 
 
-/*
+ /*
   
 output is           *
                    ***
@@ -27,3 +26,137 @@ output is           *
                  ********
                 **********
  */
+
+ for ( int i = 0; i < n ; i++){
+    // space
+    for( int j = 0; j < i; j++){
+        System.out.print(" ");
+    }
+    //  star
+    for(int  j = 0 ; j < 2*n -(2*i+1) ; j++){
+        System.out.print("*");
+    }   
+    // space
+    for( int j = 0; j < i; j++){
+        System.out.print(" ");
+    }
+    System.out.println();
+} 
+
+/*
+  output is       
+
+*********
+ ******* 
+  *****  
+   ***   
+    *  
+ */
+    for ( int i = 0; i < n ; i++){
+    // space
+        for( int j = 0; j < n-i-1; j++){
+        System.out.print(" ");
+        }
+        //  star
+        for(int  j = 0 ; j < 2 * i +1 ; j++){
+            System.out.print("*");
+        }
+        // space
+        for( int j = 0; j < n-i-1; j++){
+            System.out.print(" ");
+        }
+        System.out.println();
+    } 
+    for ( int i = 0; i < n ; i++){
+        // space
+        for( int j = 0; j < i; j++){
+            System.out.print(" ");
+        }
+        //  star
+        for(int  j = 0 ; j < 2*n -(2*i+1) ; j++){
+            System.out.print("*");
+        }   
+        // space
+        for( int j = 0; j < i; j++){
+            System.out.print(" ");
+        }
+        System.out.println();
+    } 
+
+   /*  output is 
+
+    *    
+   ***   
+  *****  
+ ******* 
+*********
+*********
+ ******* 
+  *****  
+   ***   
+    *   
+    */  
+
+
+
+        for ( int i = 0 ; i <= 2*n -1 ; i++){
+            int star = i;
+            if( i > n   )
+            {
+                star = 2*n - i;
+            }
+            for(int j = 1 ; j <= star ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+
+         /*   
+          
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+           */
+
+        int start = 1;
+        for( int i = 0 ; i < n ; i++){
+            if ( i %2 == 0){
+                start = 1;
+            }else {
+                start = 0;  
+            }
+
+            for(int j = 0; j<= i; j++){
+                System.out.print(start);
+                start = 1 - start; 
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        int space =  2 * ( n - 1);
+        for(int i=1; i<= n ; i++){
+            for ( int j =1; j <= i ; j++){
+                System.out.print(j);
+            }
+            for ( int j =1; j<= space ; j++){
+                System.out.print(" ");
+            }
+            for ( int j = i; j>= 1 ; j--){
+                System.out.print(j);
+            }
+            System.out.println();
+            space -= 2;
+        }
+
+
+    }
+}
