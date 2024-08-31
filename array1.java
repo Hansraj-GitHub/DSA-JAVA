0bba1ab5248b1b73ce9201e0ef3b60f9f8950110
Hansraj-GitHub <hraj2662001@gmail.com>
2024-08-31 12:01:31 +0530

    questions

--- array1.java
+++
@@ -1,8 +0,0 @@
-public class array1 {
-    public static void main(String[] args) {
-        int[] arr = { 5,4,3,2,1};
-        System.out.println(arr.length);
-        System.out.println(arr[2]);
-    }
-    
-}
