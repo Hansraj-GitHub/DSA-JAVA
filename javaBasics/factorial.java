fb3785e955e323ac758d4116464e3544d545decc
Hansraj-GitHub <hraj2662001@gmail.com>
2024-08-31 11:54:40 +0530

    questions

--- javaBasics/factorial.java
+++ javaBasics/factorial.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-class hello{
+class factorial{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("N: ");
