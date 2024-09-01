12c089e629ae3c8b95813531e0ea593ba0ac6b85
Hansraj-GitHub <hraj2662001@gmail.com>
2024-09-01 15:33:12 +0530

    Questions

---
+++ searching/searchInsertPosition.java
@@ -0,0 +1,21 @@
+    public class searchInsertPosition{
+        public static void main(String[] args) {
+            int[] nums = {1,3,5,6}; 
+            int target = 3;
+            int index = searchInsert(nums, target);
+            System.out.println(index);
+        }
+
+        public static int searchInsert(int[] nums, int target) {
+                for (int i = 0; i < nums.length; i++) {
+                    if (nums[i] == target) {
+                        return i;
+                    }
+                    if (nums[i] > target) {
+                        return i;
+                    }
+                }
+                return nums.length;
+        }
+    }
+    // output : 1
