e04ed92e1b23672523bbb199555bf785a3e1ef66
Hansraj-GitHub <hraj2662001@gmail.com>
2024-09-04 15:30:33 +0530

    Questions

---
+++ Arrays/singleNumber.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class singleNumber{
+    public static void main(String[] args) {
+        System.out.print("Enter Size of an array :");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print("Enter Elements :");
+
+        // Initialize the array
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+          
+        // Read the array elements of the array
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+    
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            result ^= arr[i];
+        }
+        System.out.println("Enter the single Elements :" + result);
+    }
+}
