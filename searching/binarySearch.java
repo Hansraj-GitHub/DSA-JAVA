29d4edaa10fbc8e5b3130dfd0421b1892c064699
Hansraj-GitHub <hraj2662001@gmail.com>
2024-09-19 09:48:40 +0530

    Questions

--- searching/binarySearch.java
+++ searching/binarySearch.java
@@ -1,8 +1,8 @@
 
 public class binarySearch {
     public static void main(String[] args) {
-        int[] arr = {1,5,6,7,9};
-        int target = 10;
+        int[] arr = {-1,5,6,7,9,15};
+        int target = 9;
 
         int index = -1;
         for ( int i = 0; i < arr.length ; i++){
