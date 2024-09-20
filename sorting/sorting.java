a9412c3c0251069223d6d471787dc1f0cb762de9
Hansraj-GitHub <hraj2662001@gmail.com>
2024-09-20 17:57:40 +0530

    Questions

--- sorting/sorting.java
+++ sorting/sorting.java
@@ -1,13 +1,11 @@
 public class sorting {
     public static void main(String[] args) {
-        int n = 4;
-        int arr[] = { 2, 1, 25, 15 };
+        int arr[] = { 3,1, 2, 25, 15 };
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 int temp = 0;
                 if (arr[j] < arr[i]) {
-
                     temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
