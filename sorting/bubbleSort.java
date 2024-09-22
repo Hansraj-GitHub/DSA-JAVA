f1f415f86a105168b9e1cd4d31337c45d7c41a5f
Hansraj-GitHub <hraj2662001@gmail.com>
2024-09-22 07:33:55 +0530

    Questions

--- sorting/bubbleSort.java
+++ sorting/bubbleSort.java
@@ -22,12 +22,17 @@ public class bubbleSort{
 
      public static void bubble(int[] arr){
         for ( int i = arr.length-1 ; i > 0 ; i--){
+            int didswap = 0;
             for (int j = 0 ; j < i; j++){
                 if (arr[j] > arr[j+1]){
                     int temp = arr[j+1];
                     arr[j+1] = arr[j];
                     arr[j] = temp;
+                    didswap = 1;
                 }
+            }
+            if (didswap == 0){
+                break;
             }
         }
 
