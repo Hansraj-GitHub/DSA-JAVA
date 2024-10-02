90518f5ccfd992bc8f427b04462a4bd51d20fca0
Hansraj <hraj2662001@gmail.com>
2024-10-02 12:03:40 +0530

    initial commit

--- Arrays/2D_Arrays/matrix.java
+++ Arrays/2D_Arrays/matrix.java
@@ -6,7 +6,7 @@ public class matrix {
             {7,8,9}
         };
 
-        for( int i = 0; i > arr.length ; i++){
+        for( int i = 0; i < arr.length ; i++){
             for ( int j = 0; j < arr[i].length; j++){
                 System.out.print(arr[i][j]+ " ");
             }
