0fd6d7c1279f33780bb12f42d594c20a82527cd8
Hansraj <hraj2662001@gmail.com>
2025-02-12 09:59:25 +0530

    nonReapeating character

---
+++ Strings/nonRepeatingCharacter.java
@@ -0,0 +1,29 @@
+    import java.util.*;
+
+    public class nonRepeatingCharacter {
+        public static char firstNonRepeating(String s) {
+        Map<Character, Integer> charCount = new LinkedHashMap<>();
+
+        for (char c : s.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : charCount.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+
+        return '$'; 
+    }
+
+    public static void main(String[] args) {
+        String s = "geeksforgeeks";
+       
+
+        System.out.println(firstNonRepeating(s));
+        
+    }
+}
+
+
